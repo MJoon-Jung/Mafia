92a60b8e6169a9e4033b580f493542d0b6b5acc6
MJoon-Jung <gjgjajaj31@gmail.com>
2022-10-18 11:56:54 +0900

    기존 exception들을 Custom Ws Exception으로 분리 Game gateway에 exception filter 적용

---
+++ src/modules/gateway/game/exception/index.ts
@@ -0,0 +1,5 @@
+export * from './ADeadPlayerException';
+export * from './IsNotPlayerException';
+export * from './ForbiddenDoctorSkillException';
+export * from './ForbiddenMafiaSkillException';
+export * from './ForbiddenPoliceSkillException';
